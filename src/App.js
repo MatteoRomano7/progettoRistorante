7a71f5e54ea6a1cec8af84b754c3c5cdf398e5cf
Matteo <ezmatteo@aol.com>
2023-12-20 10:57:23 +0100

    Aggiunta del modulo prenotazione al redering della pagina

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import CustomNavbar from './components/CustomNavbar';
-import Home from './components/Home';
-import MyForm1 from './components/MyForm1';
-import MyForm2 from './components/MyForm2';
-
+import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import CustomNavbar from "./components/CustomNavbar"
+import Home from "./components/Home"
+import MyForm1 from "./components/MyForm1"
+import MyForm2 from "./components/MyForm2"
+import Prenotazione from "./components/Prenotazione"
 
 function App() {
-	return (
-		<div>
-			<CustomNavbar testoAddizionale="Il nostro ristorante epico!" />
-			<Home />
-			<hr />
-			<MyForm1 />
-			<MyForm2 />
-		</div>
-	);
+  return (
+    <div>
+      <CustomNavbar testoAddizionale="Il nostro ristorante epico!" />
+      <Home />
+      <hr />
+      <MyForm1 />
+      <MyForm2 />
+      <Prenotazione />
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/components/Prenotazione.jsx
+++ src/components/Prenotazione.jsx
@@ -1,18 +1,18 @@
 import React from "react"
-import { Row, Col, Conntainer, Form, Button, Alert } from "react-bootstrap"
+import { Row, Col, Container, Form, Button, Alert } from "react-bootstrap"
 
-//Quali campi usiamo per la prentoazione?
-// 1. nome string (obbligatorio)
-// 2. telefono string (obbligatorio)
-// 3. persone number (valore predefinito:1)
-// 4. fumatori boolean
-// 5. data e ora: string (obbligatorio)
-// 6. note: string
+/* Quali campi usiamo per la prenotazione?
+1- nome: string obbligatorio
+2- telefono: string obbligatorio
+3- persone: number valore predefinito: 1
+4- fumatori: boolean
+5- data e ora: string obbligatorio
+6- note: string
+*/
 
 class Prenotazione extends React.Component {
   constructor(props) {
     super(props)
-
     this.state = {
       prenotazione: {
         nome: "",
@@ -22,33 +22,32 @@ class Prenotazione extends React.Component {
         dataOra: "",
         note: "",
       },
+      showAlert: false,
     }
-
-    this.showAlert = false
   }
-  //con la arrow fuction sottostante, non c'é bisogno di fare il bind nel constructor
 
-  handleInputChange = (proprietá, valore) => {
+  handleInputChange = (proprieta, valore) => {
     this.setState({
-      prenotazione: { ...this.setState.prenotazione, [proprietá]: valore },
+      prenotazione: { ...this.state.prenotazione, [proprieta]: valore },
     })
-    //Se vogliamo utilizzare un paramentro o una variabile come nome di una proprietá dobbiamo valutare il contenuto del parametro o della variabile con le parentesi quadre. In questo caso "proprietá" puó essere nome, telefono, persone....
+    // Se vogliamo utilizzare un parametro o una variabile come NOME di una proprietà di un oggetto dobbiamo valutare il contenuto del parametro o della variabile con le quadre []. In questo caso proprieta può essere nome, telefono, persone...
   }
 
   handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefaut()
     console.log("Invio prenotazione")
-    //Fetch con metodo POST
+    // Fetch con metodo POST
     try {
       const res = await fetch(
         "https://striveschool-api.herokuapp.com/api/reservation",
         {
           method: "POST",
           body: JSON.stringify(this.state.prenotazione),
-          headers: { "Content-Type": "Application/json" },
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
       )
-
       if (res.ok) {
         this.setState({
           prenotazione: {
@@ -68,17 +67,27 @@ class Prenotazione extends React.Component {
       console.log("Errore:", error)
     }
   }
+
   render() {
-    ;<Conntainer>
-      <Row className="justify-content-center mt-3">
-        <Col md={6}>
-          <h2 className="text-center">Modulo di prenotazione</h2>
-          {this.state.showAlert === true && (
-            <Alert variant="info">Prenotazione salvata!</Alert>
-          )}
-        </Col>
-      </Row>
-    </Conntainer>
+    return (
+      <Container>
+        <Row className="justify-content-center mt-3">
+          <Col md={6}>
+            <h2 className="text-center">Modulo di prenotazione</h2>
+            {/* {this.state.showAlert === true && (
+                            <Alert variant="info">Prenotazione salvata!</Alert>
+                        )} */}
+            <Alert
+              variant="info"
+              className={this.state.showAlert ? "d-block" : "d-none"}
+            >
+              Prenotazione salvata!
+            </Alert>
+          </Col>
+        </Row>
+      </Container>
+    )
   }
 }
+
 export default Prenotazione
