8ff959404fdba7775c335a4e81237507a1b1d1b6
Matteo <ezmatteo@aol.com>
2023-12-21 16:47:15 +0100

    aggiunto component ListaPrenotazioni

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import "./App.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 import CustomNavbar from "./components/CustomNavbar"
 import Home from "./components/Home"
-import MyForm1 from "./components/MyForm1"
-import MyForm2 from "./components/MyForm2"
 import Prenotazione from "./components/Prenotazione"
+import ListaPrenotazioni from "./components/ListaPrenotazioni"
 
 function App() {
   return (
     <div>
       <CustomNavbar testoAddizionale="Il nostro ristorante epico!" />
       <Home />
-      <hr />
       <Prenotazione />
     </div>
   )
